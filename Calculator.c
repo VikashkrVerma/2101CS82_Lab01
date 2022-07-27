4d6d5deeabfd65b0a77b83f1b48a9d9ce05f3663
VikashkrVerma <95168790+VikashkrVerma@users.noreply.github.com>
2022-07-27 23:48:36 +0530

    Exponentiation

--- Calculator.c
+++ Calculator.c
@@ -6,8 +6,11 @@ int main()
    printf("Enter Two nos ");
    int a,b;
    scanf("%d%d",&a,&b);
-   int c = a/b;
-   printf("Result is %d",c);
+   int c =1;
+   for(int i=0;i<b;i++){
+      c=c*a;
+   }
+      printf("Result is %d",c);
 
     return 0;
 }
