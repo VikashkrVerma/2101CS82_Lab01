bd2a9131985e183219e8578f9af4f0e947361a8a
VikashkrVerma <95168790+VikashkrVerma@users.noreply.github.com>
2022-07-28 00:08:39 +0530

    Percent Calculator

--- Calculator.c
+++ Calculator.c
@@ -6,11 +6,9 @@ int main()
    printf("Enter Two nos ");
    int a,b;
    scanf("%d%d",&a,&b);
-   int c =1;
-   for(int i=0;i<b;i++){
-      c=c*a;
-   }
-      printf("Result is %d",c);
+   int c = (a/b)*100;
+
+      printf("Result is %d percent",c);
 
     return 0;
 }
